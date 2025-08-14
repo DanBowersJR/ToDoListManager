4ec4ec6197282d9d8617714f61f6622ac77263f5
Daniel Bowers <34407755+DanBowersJR@users.noreply.github.com>
2025-08-14 01:21:55 -0230

    Updated Main to use array of users and meet project requirements

--- src/com/todolistmanager/Main.java
+++ src/com/todolistmanager/Main.java
@@ -2,22 +2,28 @@ package com.todolistmanager;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User("Daniel");
+        // Create array of users
+        User[] users = new User[2];
+        users[0] = new User("Daniel");
+        users[1] = new User("Alice");
 
-        // Add tasks
-        user.getTaskList().addTask("Finish Java homework");
-        user.getTaskList().addTask("Go grocery shopping");
-        user.getTaskList().addTask("Workout for 30 minutes");
+        // Add tasks for Daniel
+        users[0].getTaskList().addTask("Finish Java homework");
+        users[0].getTaskList().addTask("Go grocery shopping");
+        users[0].getTaskList().addTask("Workout for 30 minutes");
 
-        // Print tasks
-        System.out.println("Tasks for " + user.getName() + ":");
-        user.getTaskList().printTasks();
+        // Add tasks for Alice
+        users[1].getTaskList().addTask("Call the bank");
+        users[1].getTaskList().addTask("Pay electricity bill");
 
-        // Complete a task
-        user.getTaskList().markTaskCompleted(1);
+        // Mark one of Daniel's tasks as completed
+        users[0].getTaskList().markTaskCompleted(1); // Mark "Go grocery shopping" done
 
-        // Print tasks again
-        System.out.println("\nAfter marking one task as completed:");
-        user.getTaskList().printTasks();
+        // Print all tasks for each user
+        for (User user : users) {
+            System.out.println("Tasks for " + user.getName() + ":");
+            user.getTaskList().printTasks();
+            System.out.println();
+        }
     }
 }
